80e3be4c295f4fe07c1a0863dd94b279e9557aaf
GitSKY9795 <sky9795000190@gmail.com>
2026-02-18 19:36:03 +0530

    18th feb

---
+++ context-api/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav2 from './components/Nav2';
+import Navbar from './components/Navbar';
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+     
+    </div>
+  )
+}
+
+export default App
